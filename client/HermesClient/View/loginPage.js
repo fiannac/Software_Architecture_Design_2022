eeea5edac1c9124cc4ffc90bc8d141fa371c3c77
mconti99 <matteo_conti99@virgilio.it>
2022-06-26 16:31:39 +0200

    aggiornamento view

--- client/HermesClient/View/loginPage.js
+++ client/HermesClient/View/loginPage.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { touchProps } from 'react-native-web/dist/cjs/modules/forwardedProps';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import {setConnState} from '../App.js'
 
@@ -9,6 +10,7 @@ export default class LoginPage extends React.Component {
   constructor(prop){
     super()
     this.state = {
+      hidePass: true,
     }
     console.log(prop.route.params.val)
     this.navigation = prop.navigation;
@@ -21,12 +23,54 @@ export default class LoginPage extends React.Component {
     return (
       <View>
         <Text>Effettua il login o registrati!</Text>
-        <TextInput onChangeText = {(value) => {this.usrname = value}} />
-        <TextInput onChangeText = {(value) => {this.psw = value}} secureTextEntry = {true} />
-        <Button title="Log in" onPress = {()=>setConnState()}/>
-        <Button title="Crea Nuovo account" onPress={() => this.navigation.push('RegisterPage')}/>
+        <TextInput onChangeText = {(value) => {this.usrname = value}} style={styles.inserimentoTestoUser} placeholder="Inserisci nome utente"/>
+        <TextInput onChangeText = {(value) => {this.psw = value}} style={styles.inserimentoTestoPsw} placeholder="Inserisci password" autoCompleteType="password" secureTextEntry={this.state.hidePass ? true : false}/>
+        <Icon
+          name={this.state.hidePass ? 'eye-slash' : 'eye'}
+          size={15}
+          color="grey"
+          onPress={() => this.setState({hidePass: !this.state.hidePass})}
+        />
+        <TextInput/>
+        <Button title="Log in" onPress = {()=>setConnState()} style={styles.bottone} />
+        <Button title="Crea Nuovo account" onPress={() => this.navigation.push('RegisterPage')} style={styles.bottone}/>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  inserimentoTestoUser: {
+    borderWidth: 1,
+    marginVertical: 5,
+    width: 200,
+    marginHorizontal: 50,
+    alignSelf: 'center',
+    textAlign: 'center',
+    color: 'black',
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  inserimentoTestoPsw: {
+    borderWidth: 1,
+    marginVertical: 5,
+    width: 200
+  },
+  bottone: {
+    flexDirection: "row",//allineo sull'asse x
+    marginVertical: 5,
+  }
+});
--- client/HermesClient/View/registrationPage.js
+++ client/HermesClient/View/registrationPage.js
@@ -5,7 +5,7 @@ import { touchProps } from 'react-native-web/dist/cjs/modules/forwardedProps';
 
 
 export default class RegistrationPage extends React.Component {
-  constructor(){
+  constructor(prop){
     super()
     this.usrname = ''
     this.psw = ''
@@ -16,13 +16,34 @@ export default class RegistrationPage extends React.Component {
     return (
         <View>
         <Text>Registrati con username e password</Text>
-        <TextInput onChangeText = {(value) => {this.mail = value}} />
-        <TextInput onChangeText = {(value) => {this.usrname = value}} />
-        <TextInput onChangeText = {(value) => {this.psw = value}} secureTextEntry = {true} />
-
-        <Button title="Registrati!" onPress={() => console.log("zio pera")}/>
+        <TextInput onChangeText = {(value) => {this.mail = value}} style={styles.inserimentoTestoMail} placeholder="Inserisci mail"/>
+        <TextInput onChangeText = {(value) => {this.usrname = value}} style={styles.inserimentoTestoUser} placeholder='Inserisci nome utente'/>
+        <TextInput onChangeText = {(value) => {this.psw = value}} secureTextEntry = {true} style={styles.inserimentoTestoPsw} placeholder='Inserisci password'/>
+        
+        <Button title="Registrati!" onPress={() => console.log("zio pera")} style={styles.bottone}/>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  inserimentoTestoUser: {
+    borderWidth: 1,
+    marginVertical: 5,
+    width: 200
+  },
+  inserimentoTestoPsw: {
+    borderWidth: 1,
+    marginVertical: 5,
+    width: 200
+  },
+  inserimentoTestoMail: {
+    borderWidth: 1,
+    marginVertical: 5,
+    width: 200
+  },
+  bottone: {
+    flexDirection: "row",//allineo sull'asse x
+    marginVertical: 5,
+  }
+});
