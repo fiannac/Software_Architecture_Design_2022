c37e32e031c75b86b5362d990ef98b630d080fb2
alfonsoconte <alfonso.conte2009@live.it>
2022-06-27 20:05:54 +0200

    Update crypto file
    Aggiunta delle funzioni di cript e decript

--- client/HermesClient/Services/crypto.js
+++ client/HermesClient/Services/crypto.js
@@ -1,29 +1,58 @@
 import { NativeModules, Platform } from 'react-native'
 
 
+import RSAKey from 'react-native-rsa';
+const bcrypt = require('bcrypt');
+var CryptoJS = require("crypto-js");
+
 
 export default class Crypto {
-    encryptPrk(password, prk){
-        return prk
+
+    constructor(){
+        this.rsa = new RSAKey();
     }
-    decryptPrk(password, prk){
-        return prk
+    
+    encryptPrk(prk, password){ // prk è ciò che viene cifrato con la password
+    
+        var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(password), prk).toString();
+        console.log(ciphertext);
+        return ciphertext;
     }
-    hashPsw(psw){
-        return psw
+    decryptPrk(ciphertext, password){ //cifr
+
+        var bytes = CryptoJS.AES.decrypt(ciphertext, password);
+        var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+        return decryptedData;
     }
+
+
+ù
+    async hashPsw(psw){
+    const salt = await bcrypt.genSalt(6);
+    const hashed = await bcrypt.hash(psw, salt);
+    return hashed;
+    }
+
+    
     generateKeys(){
-        const keys = {
-            prk: 0,
-            puk: 0
-        }
-        return keys
+    const bits = 1024;
+    const exponent = '10001'; // must be a string. This is hex string. decimal = 65537
+    this.rsa.generate(bits, exponent);
+    var publicKey = this.rsa.getPublicString(); // return json encoded string
+    var privateKey = this.rsa.getPrivateString(); // return json encoded string
+    return {publicKey,privateKey};
     }
+
     encryptMsg(msg, puk){
-        return msg
+    this.rsa.setPublicString(puk);
+    return this.rsa.encrypt(msg);
     }
+
     decryptMsg(msg, prk){
-        return msg
+    this.rsa.setPrivateString(prk);
+    return  this.rsa.decrypt(msg); // decrypted == originText
     }
+
+
 
 }
